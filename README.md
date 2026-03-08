b46a1cf23d592db51c3751c6e1a219f5836dd7a8
HonourJAH <onojahmicheal@gmail.com>
2026-03-08 21:24:28 +0100

    todo-list completed

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # to-do-list
+
+## Through this project, I have practiced:
+
+DOM manipulation,
+
+Event delegation,
+
+Array methods,
+
+Objects,
+
+localStorage,
+
+JSON.stringify and
+
+JSON.parse
--- script.js
+++ script.js
@@ -4,27 +4,85 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const container = document.querySelector(".container");
 
-addTaskBtn.addEventListener("click", function () {
-  let taskInput = document.getElementById("taskInput");
-  // console.log(taskInput);
-  if (!taskInput.value) alert("Please, enter a task.");
-  else {
-    const html = `
-    <li>
-    <span>${taskInput.value}</span>
-    <button class="delete-btn">Delete</button>
-    </li>
+function saveTasks() {
+  const tasks = [];
+
+  document.querySelectorAll("#taskList li").forEach((task) => {
+    tasks.push({
+      text: task.querySelector("span").textContent,
+      completed: task.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+
+    if (task.completed) {
+      li.classList.add("completed");
+    }
+
+    li.innerHTML = `
+      <span>${task.text}</span>
+      <button class="delete-btn">Delete</button>
     `;
 
-    taskInput.value = "";
-    taskList.insertAdjacentHTML("beforeend", html);
+    document.getElementById("taskList").appendChild(li);
+  });
+}
+
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+
+  if (!taskInput.value) {
+    alert("Please, enter a task.");
+    return;
   }
+
+  const html = `
+    <li>
+      <span>${taskInput.value}</span>
+      <button class="delete-btn">Delete</button>
+    </li>
+  `;
+
+  taskList.insertAdjacentHTML("beforeend", html);
+
+  taskInput.value = "";
   taskInput.focus();
-});
+
+  saveTasks();
+}
 
 container.addEventListener("click", function (e) {
   const target = e.target.classList.contains("delete-btn");
   if (!target) return;
 
   e.target.parentElement.remove();
+  saveTasks();
+});
+
+container.addEventListener("click", function (e) {
+  const target = e.target.tagName === "SPAN";
+  if (!target) return;
+
+  e.target.parentElement.classList.toggle("completed");
+  saveTasks();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadTasks();
+});
+
+addTaskBtn.addEventListener("click", addTask);
+
+taskInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
 });
